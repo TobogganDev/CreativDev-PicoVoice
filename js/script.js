90be7d193ea78b81129b409224636b86acc5b82f
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-04-19 09:57:47 +0200

    :sparkles: voice recognition

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const results = document.getElementById('results');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'fr-FR';
+recognition.continuous = true;
+
+function highlightVerbs(text) {
+  const verbs = ['être', 'parler', 'faire', 'dire', 'aller'];
+  const words = text.split(' ');
+  return words.map(word => {
+    if (verbs.includes(word)) {
+      return `<span class="verb">${word}</span>`;
+    }
+    return word;
+  }).join(' ');
+}
+
+startButton.addEventListener('click', () => {
+  recognition.start();
+});
+
+stopButton.addEventListener('click', () => {
+  recognition.stop();
+});
+
+recognition.addEventListener('result', event => {
+  const transcript = Array.from(event.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+  const formattedText = highlightVerbs(transcript);
+  results.innerHTML = formattedText;
+});
